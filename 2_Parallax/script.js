cca188f9e0f918a79a322c02cc3912326c41a642
Guohui Guan <guohui_guan@berkeley.edu>
2022-11-03 18:48:56 -0700

    optimize by deleting x2

--- 2_Parallax/script.js
+++ 2_Parallax/script.js
@@ -31,29 +31,27 @@ class Layer {
     this.y = 0;
     this.width = 2400;
     this.height = 700;
-    this.x2 = this.width;
+    // this.x2 = this.width;
     this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
   }
   update() {
     this.speed = gameSpeed * this.speedModifier;
-    if (this.x < -this.width) this.x = this.width + this.x2 - this.speed;
-    if (this.x2 < -this.width) this.x2 = this.width + this.x - this.speed;
-    this.x = Math.floor(this.x - this.speed);
-    this.x2 = Math.floor(this.x2 - this.speed);
+    if (this.x < -this.width) this.x = 0;
+    this.x = this.x - this.speed;
   }
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
-
-// const layer1 = new Layer(backgoundLayer4, 0.5);
-// const layer2 = new Layer(backgoundLayer4, 0.7);
-// const layer3 = new Layer(backgoundLayer4, 0.8);
-// const layer4 = new Layer(backgoundLayer4, 0.9);
-// const layer5 = new Layer(backgoundLayer4, 1);
 
 const gameObjects = [
   new Layer(backgoundLayer1, 0.2),
